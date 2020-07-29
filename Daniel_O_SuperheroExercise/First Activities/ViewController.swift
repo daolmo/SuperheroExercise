1141e89a01254165d2770ae3be1243a41d739367
daolmo <67968940+daolmo@users.noreply.github.com>
2020-07-29 11:19:58 -0500

    First commit
    First commit

---
+++ Daniel_O_SuperheroExercise/First Activities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Activities
+//
+//  Created by Daniel Olmo on 7/29/20.
+//  Copyright © 2020 Daniel Olmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Daniel_O_SuperheroExercise/First ActivitiesTests/First_ActivitiesTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_ActivitiesTests.swift
+//  First ActivitiesTests
+//
+//  Created by Daniel Olmo on 7/29/20.
+//  Copyright © 2020 Daniel Olmo. All rights reserved.
+//
+
+import XCTest
+@testable import First_Activities
+
+class First_ActivitiesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
